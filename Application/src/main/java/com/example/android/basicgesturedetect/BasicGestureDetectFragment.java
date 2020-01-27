36c40e8d9c3547f21fb7b3cb383f2ae29d084abd
Hans Christian Falkenberg <flixus@gmail.com>
2020-01-27 13:32:13 +0100

    Apply new AndroidX package names

--- Application/src/main/java/com/example/android/basicgesturedetect/BasicGestureDetectFragment.java
+++ Application/src/main/java/com/example/android/basicgesturedetect/BasicGestureDetectFragment.java
@@ -18,7 +18,7 @@ package com.example.android.basicgesturedetect;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.GestureDetector;
 import android.view.MenuItem;
 import android.view.MotionEvent;
